5ce054f150452a1070b7019b2a60864ed8d83c33
Wanji Kay <cirukins@hotmail.com>
2025-10-07 21:22:58 -0400

    created how index new htmls

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
   get "tasks", to: "tasks#index"
+  get "task/:id", to: "tasks#show", as: :task
+  get "tasks/new", to: "tasks#new"
+  post "tasks", to: "tasks#create"
 end
