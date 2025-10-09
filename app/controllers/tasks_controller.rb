3f4009f2105b25c419f28c2b28de88616050e7a8
Wanji Kay <cirukins@hotmail.com>
2025-10-09 19:32:43 -0400

    created destroy method and edit.html.erb

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,5 @@
 class TasksController < ApplicationController
+
   def index
     @tasks = Task.all
   end
@@ -13,9 +14,23 @@ class TasksController < ApplicationController
 
   def create
     @task = Task.new(task_params)
-    raise
     @task.save
-    redirect_to task_path(@tasks)
+    redirect_to task_path(@task)
+  end
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+    redirect_to task_path(@task)
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to restaurants_path, status: :see_other
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,10 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
   get "tasks", to: "tasks#index"
-  get "task/:id", to: "tasks#show", as: :task
-  get "tasks/new", to: "tasks#new"
+  get "tasks/new", to: "tasks#new", as: :new_task
   post "tasks", to: "tasks#create"
+  get "tasks/:id", to: "tasks#show", as: :task
+  get "tasks/:id/edit", to: "tasks#edit"
+  patch "tasks/:id", to: "tasks#update"
+  delete "tasks/:id", to: "tasks#destroy"
 end
